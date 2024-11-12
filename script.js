a1167a67a240337e2c51ec937d0d147188645154
boris2442 <aubinborissimotsebo@gmail.com>
2024-11-12 12:47:59 +0100

    change text

---
+++ script.js
@@ -0,0 +1,12 @@
+// const question=document.querySelector("button");
+// const para=document.querySelector("p");
+// question.addEventListener("click", ()=>{
+//    question.classList.toggle("response");
+//    para.style.visibility="visible";
+   
+// })
+
+const answ1=document.querySelector(".btn1, .btn2, .btn3, .btn4");
+answ1.addEventListener("click", ()=>{
+    alert('hello');
+})
