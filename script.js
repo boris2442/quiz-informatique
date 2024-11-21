e1dd0682a2129e4048ffa5d291e32d7ac7f4526b
boris2442 <aubinborissimotsebo@gmail.com>
2024-11-21 21:33:15 +0100

    introduction aux javascript

--- script.js
+++ script.js
@@ -5,8 +5,24 @@
 //    para.style.visibility="visible";
    
 // })
-
-const answ1=document.querySelector(".btn1, .btn2, .btn3, .btn4");
-answ1.addEventListener("click", ()=>{
-    alert('hello');
+const answ1=document.querySelector(".answ1")
+const btn1=document.querySelector(" .btn1");
+btn1.addEventListener("click", ()=>{
+  answ1.classList.toggle("ans1");
+ 
+ 
 })
+const btn2=document.querySelector(".btn2");
+btn2.addEventListener("click", ()=>{
+    alert("bravo à vous! bonne reponse");
+   
+  })
+  const btn3=document.querySelector(".btn3");
+  btn3.addEventListener("click",() =>{
+     answ1.classList.toggle("ans1")
+  
+  })
+  const btn4=document.querySelector(".btn4");
+  btn4.addEventListener("click", ()=>{
+    answ1.classList.toggle("ans1");
+  })
