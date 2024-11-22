a8400e5c83ac208cc06931492bc9f8c82de208fc
boris2442 <aubinborissimotsebo@gmail.com>
2024-11-22 16:11:19 +0100

    suite javascript

---
+++ javascript/index.js
@@ -0,0 +1,41 @@
+
+// 26e question=>1er question partie programmation
+const answer26=document.querySelector(".answ26");
+const button105=document.querySelector("#btn105");
+button105.addEventListener("click", ()=>{
+    answer26.classList.toggle("ans4")
+})
+const button106=document.querySelector("#btn106");
+button106.addEventListener("click", ()=>{
+    alert("Bravo a vous! Bonne reponse");
+})
+const button107=document.querySelector("#btn107");
+button107.addEventListener("click", ()=>{
+    answer26.classList.toggle("ans4");
+})
+const button108=document.querySelector("#btn108");
+button108.addEventListener("click", ()=>{
+    answer26.classList.toggle("ans4");
+})
+//2e reponse programmation
+
+
+const answer27=document.querySelector(".answ27");
+const button109=document.querySelector("#btn109");
+button109.addEventListener("click", ()=>{
+    answer27.classList.toggle("ans4")
+})
+const button110=document.querySelector("#btn110");
+button110.addEventListener("click", ()=>{
+    alert("Bravo a vous! Bonne reponse");
+})
+const button111=document.querySelector("#btn111");
+button111.addEventListener("click", ()=>{
+    answer27.classList.toggle("ans4");
+})
+const button112=document.querySelector("#btn112");
+button112.addEventListener("click", ()=>{
+    answer27.classList.toggle("ans4");
+})
+
+// 3e question programmation
