bf3c10f310920f20277f0f289de6db6dd0bf43ff
boris2442 <aubinborissimotsebo@gmail.com>
2024-11-24 02:41:56 +0100

    text

--- javascript/script2.js
+++ javascript/script2.js
@@ -1,5 +1,107 @@
-const answer52=document.querySelector(".answ52");
+// first answer bases of donnes
+const answer51=document.querySelector(".answ51");
+
+
 const button191=document.querySelector("#btn191");
 const button192=document.querySelector("#btn192");
 const button193=document.querySelector("#btn193");
 const button194=document.querySelector("#btn194");
+button191.addEventListener("click", ()=>{
+   answer51.classList.toggle("ans4");
+})
+button192.addEventListener("click", ()=>{
+    alert("Bravo à vous! Bonne reponse")
+})
+button193.addEventListener("click", ()=>{
+    answer51.classList.toggle("ans4");
+ })
+button194.addEventListener("click", ()=>{
+    answer51.classList.toggle("ans4");
+ })
+
+ //the third answer of bases of donnees
+
+ const answer53=document.querySelector(".answ53");
+const button195=document.querySelector("#btn195");
+const button196=document.querySelector("#btn196");
+const button197=document.querySelector("#btn197");
+const button198=document.querySelector("#btn198");
+
+button195.addEventListener("click", ()=>{
+answer53.classList.toggle("ans4");
+})
+button196.addEventListener("click", ()=>{
+    alert("Bravo à vous! Bonne reponse")
+})
+button197.addEventListener("click", ()=>{
+    answer53.classList.toggle("ans4");
+ })
+button198.addEventListener("click", ()=>{
+
+    answer53.classList.toggle("ans4");
+ })
+//the fourth answer of bases of donnees
+
+const answer54=document.querySelector(".answ54");
+const button199=document.querySelector("#btn199");
+const button200=document.querySelector("#btn200");
+const button201=document.querySelector("#btn201");
+const button202=document.querySelector("#btn202");
+
+button199.addEventListener("click", ()=>{
+answer54.classList.toggle("ans4");
+})
+button201.addEventListener("click", ()=>{
+    alert("Bravo à vous! Bonne reponse")
+})
+button200.addEventListener("click", ()=>{
+    answer54.classList.toggle("ans4");
+ })
+button202.addEventListener("click", ()=>{
+
+    answer54.classList.toggle("ans4");
+ })
+
+ //the fifth answer of bases of donnees
+
+const answer55=document.querySelector(".answ55");
+const button203=document.querySelector("#btn203");
+const button204=document.querySelector("#btn204");
+const button205=document.querySelector("#btn205");
+const button206=document.querySelector("#btn206");
+
+button204.addEventListener("click", ()=>{
+answer55.classList.toggle("ans4");
+})
+button203.addEventListener("click", ()=>{
+    alert("Bravo à vous! Bonne reponse")
+})
+button205.addEventListener("click", ()=>{
+    answer55.classList.toggle("ans4");
+ })
+button206.addEventListener("click", ()=>{
+
+    answer55.classList.toggle("ans4");
+ })
+
+ //the fifth answer of bases of donnees
+
+const answer56=document.querySelector(".answ55");
+const button207=document.querySelector("#btn207");
+const button208=document.querySelector("#btn208");
+const button209=document.querySelector("#btn209");
+const button210=document.querySelector("#btn210");
+
+button204.addEventListener("click", ()=>{
+answer55.classList.toggle("ans4");
+})
+button203.addEventListener("click", ()=>{
+    alert("Bravo à vous! Bonne reponse")
+})
+button205.addEventListener("click", ()=>{
+    answer55.classList.toggle("ans4");
+ })
+button206.addEventListener("click", ()=>{
+
+    answer55.classList.toggle("ans4");
+ })
