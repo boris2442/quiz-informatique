ec1f14ac2e78ce1aa2e3167552f35a633cba0844
boris2442 <aubinborissimotsebo@gmail.com>
2024-11-24 01:52:17 +0100

    debugger

---
+++ javascript/script2.js
@@ -0,0 +1,5 @@
+const answer52=document.querySelector(".answ52");
+const button191=document.querySelector("#btn191");
+const button192=document.querySelector("#btn192");
+const button193=document.querySelector("#btn193");
+const button194=document.querySelector("#btn194");
