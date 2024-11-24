2a04aa433e31fcacb5e6ef9ab1f57ab56c02a658
boris2442 <aubinborissimotsebo@gmail.com>
2024-11-24 15:35:38 +0100

    finish javascript

--- javascript/script.js
+++ javascript/script.js
@@ -50,25 +50,25 @@ button8.addEventListener("click", ()=>{
 const answer3=document.querySelector(".answ3");
 const button11=document.querySelector("#btn11");
   button11.addEventListener("click", ()=>{
-alert("Bravo à vous ! Bonne reponse")
+alert("Bravo à vous ! Bonne reponse");
   })
   const button9=document.querySelector("#btn9");
   button9.addEventListener("click",() =>{
-   answer3.classList.toggle("ans3")
+  answer3.classList.toggle("ans4");
   })
   const button10=document.querySelector("#btn10");
   button10.addEventListener("click", ()=>{
-    answer3.classList.toggle("ans3");
+    answer3.classList.toggle("ans4");
   })
   const button12=document.querySelector("#btn12");
   button12.addEventListener("click", ()=>{
-    answer3.classList.toggle("ans3");
+    answer3.classList.toggle("ans4");
   })
   //quatrieme question initiation
   const answer4=document.querySelector(".answ4");
   const button13=document.querySelector("#btn13");
   button13.addEventListener("click",()=>{
-    answer4.classList.toggle("ans3")
+    answer4.classList.toggle("ans4");
   })
   const button14=document.querySelector("#btn14");
   button14.addEventListener("click",()=>{
@@ -76,11 +76,11 @@ alert("Bravo à vous ! Bonne reponse")
 })
 const button15=document.querySelector("#btn15");
   button15.addEventListener("click",()=>{
-    answer4.classList.toggle("ans3");
+    answer4.classList.toggle("ans4");
   })
   const button16=document.querySelector("#btn16");
   button16.addEventListener("click",()=>{
-    answer4.classList.toggle("ans3");
+    answer4.classList.toggle("ans4");
   })
   //cinquieme reponse initiation
   const answer5=document.querySelector(".answ5");
@@ -91,15 +91,15 @@ button17.addEventListener("click", ()=>{
 
 const button18=document.querySelector("#btn18");
   button18.addEventListener("click",()=>{
-  answer5.classList.toggle("ans3");
+  answer5.classList.toggle("ans4");
   })
 const button19=document.querySelector("#btn19");
   button19.addEventListener("click",()=>{
-  answer5.classList.toggle("ans3");
+  answer5.classList.toggle("ans4");
   })
 const button20=document.querySelector("#btn20");
   button20.addEventListener("click",()=>{
-  answer5.classList.toggle("ans3");
+  answer5.classList.toggle("ans4");
   })
 
 //sixieme question initiation
@@ -107,7 +107,7 @@ const button20=document.querySelector("#btn20");
 const answer6=document.querySelector(".answ6");
 const button21=document.querySelector("#btn21");
 button21.addEventListener("click", ()=>{
-answer6.classList.toggle("ans3")
+answer6.classList.toggle("ans4")
 })
 const button22=document.querySelector("#btn22");
 button22.addEventListener("click", ()=>{
@@ -115,11 +115,11 @@ button22.addEventListener("click", ()=>{
 })
 const button23=document.querySelector("#btn23");
 button23.addEventListener("click", ()=>{
-answer6.classList.toggle("ans3")
+answer6.classList.toggle("ans4")
 })
 const button24=document.querySelector("#btn24");
 button24.addEventListener("click", ()=>{
-answer6.classList.toggle("ans3")
+answer6.classList.toggle("ans4")
 })
 
 //septieme question de l'initiation
@@ -127,7 +127,7 @@ answer6.classList.toggle("ans3")
 const answer7=document.querySelector(".answ7");
 const button25=document.querySelector("#btn25");
 button25.addEventListener("click", ()=>{
-answer7.classList.toggle("ans3")
+answer7.classList.toggle("ans4")
 })
 const button28=document.querySelector("#btn28");
 button28.addEventListener("click", ()=>{
@@ -135,18 +135,18 @@ button28.addEventListener("click", ()=>{
 })
 const button26=document.querySelector("#btn26");
 button26.addEventListener("click", ()=>{
-answer7.classList.toggle("ans3");
+answer7.classList.toggle("ans4");
 })
 const button27=document.querySelector("#btn27");
 button27.addEventListener("click", ()=>{
-answer7.classList.toggle("ans3");
+answer7.classList.toggle("ans4");
 })
 //huitieme question
 
 const answer8=document.querySelector(".answ8");
 const button29=document.querySelector("#btn29");
 button29.addEventListener("click", ()=>{
-answer8.classList.toggle("ans3");
+answer8.classList.toggle("ans4");
 })
 const button32=document.querySelector("#btn32");
 button32.addEventListener("click", ()=>{
@@ -154,11 +154,11 @@ button32.addEventListener("click", ()=>{
 })
 const button31=document.querySelector("#btn31");
 button31.addEventListener("click", ()=>{
-answer8.classList.toggle("ans3");
+answer8.classList.toggle("ans4");
 })
 const button30=document.querySelector("#btn30");
 button30.addEventListener("click", ()=>{
-answer8.classList.toggle("ans3");
+answer8.classList.toggle("ans4");
 });
 //neuvieme reponse initiation
 
@@ -166,7 +166,7 @@ answer8.classList.toggle("ans3");
 const answer9=document.querySelector(".answ9");
 const button33=document.querySelector("#btn33");
 button33.addEventListener("click", ()=>{
-answer9.classList.toggle("ans3");
+answer9.classList.toggle("ans4");
 });
 const button36=document.querySelector("#btn36");
 button36.addEventListener("click", ()=>{
@@ -174,18 +174,18 @@ button36.addEventListener("click", ()=>{
 });
 const button34=document.querySelector("#btn34");
 button34.addEventListener("click", ()=>{
-answer9.classList.toggle("ans3");
+answer9.classList.toggle("ans4");
 });
 const button35=document.querySelector("#btn35");
 button35.addEventListener("click", ()=>{
-answer9.classList.toggle("ans3");
+answer9.classList.toggle("ans4");
 });
 
 // dixieme reponse initiation
 const answer10=document.querySelector(".answ10");
 const button37=document.querySelector("#btn37");
 button37.addEventListener("click", ()=>{
-answer10.classList.toggle("ans3");
+answer10.classList.toggle("ans4");
 });
 const button40=document.querySelector("#btn40");
 button40.addEventListener("click", ()=>{
@@ -193,11 +193,11 @@ button40.addEventListener("click", ()=>{
 });
 const button38=document.querySelector("#btn38");
 button38.addEventListener("click", ()=>{
-answer10.classList.toggle("ans3");
+answer10.classList.toggle("ans4");
 });
 const button39=document.querySelector("#btn39");
 button39.addEventListener("click", ()=>{
-answer10.classList.toggle("ans3");
+answer10.classList.toggle("ans4");
 });
 
 // onzieme reponse initition
@@ -205,7 +205,7 @@ answer10.classList.toggle("ans3");
 const answer11=document.querySelector(".answ11");
 const button41=document.querySelector("#btn41");
 button41.addEventListener("click", ()=>{
-answer11.classList.toggle("ans3");
+answer11.classList.toggle("ans4");
 });
 const button42=document.querySelector("#btn42");
 button42.addEventListener("click", ()=>{
@@ -213,11 +213,11 @@ button42.addEventListener("click", ()=>{
 });
 const button43=document.querySelector("#btn43");
 button43.addEventListener("click", ()=>{
-answer11.classList.toggle("ans3");
+answer11.classList.toggle("ans4");
 });
 const button44=document.querySelector("#btn44");
 button44.addEventListener("click", ()=>{
-answer11.classList.toggle("ans3");
+answer11.classList.toggle("ans4");
 });
 
 // douzieme question
@@ -225,7 +225,7 @@ answer11.classList.toggle("ans3");
 const answer12=document.querySelector(".answ12");
 const button46=document.querySelector("#btn46");
 button46.addEventListener("click", ()=>{
-answer12.classList.toggle("ans3");
+answer12.classList.toggle("ans4");
 });
 const button45=document.querySelector("#btn45");
 button45.addEventListener("click", ()=>{
@@ -233,11 +233,11 @@ button45.addEventListener("click", ()=>{
 });
 const button47=document.querySelector("#btn47");
 button47.addEventListener("click", ()=>{
-answer12.classList.toggle("ans3");
+answer12.classList.toggle("ans4");
 });
 const button48=document.querySelector("#btn48");
 button48.addEventListener("click", ()=>{
-answer12.classList.toggle("ans3");
+answer12.classList.toggle("ans4");
 });
 
 
@@ -245,7 +245,7 @@ answer12.classList.toggle("ans3");
 const answer13=document.querySelector(".answ13");
 const button49=document.querySelector("#btn49");
 button49.addEventListener("click", ()=>{
-answer13.classList.toggle("ans3");
+answer13.classList.toggle("ans4");
 });
 const button51=document.querySelector("#btn51");
 button51.addEventListener("click", ()=>{
@@ -253,11 +253,11 @@ button51.addEventListener("click", ()=>{
 });
 const button50=document.querySelector("#btn50");
 button50.addEventListener("click", ()=>{
-answer13.classList.toggle("ans3");
+answer13.classList.toggle("ans4");
 });
 const button52=document.querySelector("#btn52");
 button52.addEventListener("click", ()=>{
-answer13.classList.toggle("ans3");
+answer13.classList.toggle("ans4");
 });
 
 //quatorziem question
@@ -265,7 +265,7 @@ answer13.classList.toggle("ans3");
 const answer14=document.querySelector(".answ14");
 const button53=document.querySelector("#btn53");
 button53.addEventListener("click", ()=>{
-answer14.classList.toggle("ans3");
+answer14.classList.toggle("ans4");
 });
 const button55=document.querySelector("#btn55");
 button55.addEventListener("click", ()=>{
@@ -273,11 +273,11 @@ button55.addEventListener("click", ()=>{
 });
 const button54=document.querySelector("#btn54");
 button54.addEventListener("click", ()=>{
-answer14.classList.toggle("ans3");
+answer14.classList.toggle("ans4");
 });
 const button56=document.querySelector("#btn56");
 button56.addEventListener("click", ()=>{
-answer14.classList.toggle("ans3");
+answer14.classList.toggle("ans4");
 });
 
 //Quinzieme question
@@ -285,7 +285,7 @@ answer14.classList.toggle("ans3");
 const answer15=document.querySelector(".answ15");
 const button57=document.querySelector("#btn57");
 button57.addEventListener("click", ()=>{
-answer15.classList.toggle("ans3");
+answer15.classList.toggle("ans4");
 });
 const button58=document.querySelector("#btn58");
 button58.addEventListener("click", ()=>{
@@ -293,11 +293,11 @@ button58.addEventListener("click", ()=>{
 });
 const button59=document.querySelector("#btn59");
 button59.addEventListener("click", ()=>{
-answer15.classList.toggle("ans3");
+answer15.classList.toggle("ans4");
 });
 const button60=document.querySelector("#btn60");
 button60.addEventListener("click", ()=>{
-answer15.classList.toggle("ans3");
+answer15.classList.toggle("ans4");
 });
 
 //seizieme question
@@ -305,7 +305,7 @@ answer15.classList.toggle("ans3");
 const answer16=document.querySelector(".answ16");
 const button61=document.querySelector("#btn61");
 button61.addEventListener("click", ()=>{
-answer16.classList.toggle("ans3");
+answer16.classList.toggle("ans4");
 });
 const button62=document.querySelector("#btn62");
 button62.addEventListener("click", ()=>{
@@ -313,18 +313,18 @@ button62.addEventListener("click", ()=>{
 });
 const button63=document.querySelector("#btn63");
 button63.addEventListener("click", ()=>{
-answer16.classList.toggle("ans3");
+answer16.classList.toggle("ans4");
 });
 const button64=document.querySelector("#btn64");
 button64.addEventListener("click", ()=>{
-answer16.classList.toggle("ans3");
+answer16.classList.toggle("ans4");
 });
 // 17e question
 
 const answer17=document.querySelector(".answ17");
 const button65=document.querySelector("#btn65");
 button65.addEventListener("click", ()=>{
-answer17.classList.toggle("ans3");
+answer17.classList.toggle("ans4");
 });
 const button66=document.querySelector("#btn66");
 button66.addEventListener("click", ()=>{
@@ -332,11 +332,11 @@ button66.addEventListener("click", ()=>{
 });
 const button67=document.querySelector("#btn67");
 button67.addEventListener("click", ()=>{
-answer17.classList.toggle("ans3");
+answer17.classList.toggle("ans4");
 });
 const button68=document.querySelector("#btn68");
 button68.addEventListener("click", ()=>{
-answer17.classList.toggle("ans3");
+answer17.classList.toggle("ans4");
 });
 
 //19e question
@@ -344,7 +344,7 @@ answer17.classList.toggle("ans3");
 const answer18=document.querySelector(".answ18");
 const button69=document.querySelector("#btn69");
 button69.addEventListener("click", ()=>{
-answer18.classList.toggle("ans3");
+answer18.classList.toggle("ans4");
 });
 const button71=document.querySelector("#btn71");
 button71.addEventListener("click", ()=>{
@@ -352,11 +352,11 @@ button71.addEventListener("click", ()=>{
 });
 const button70=document.querySelector("#btn70");
 button70.addEventListener("click", ()=>{
-answer18.classList.toggle("ans3");
+answer18.classList.toggle("ans4");
 });
 const button72=document.querySelector("#btn72");
 button72.addEventListener("click", ()=>{
-answer18.classList.toggle("ans3");
+answer18.classList.toggle("ans4");
 });
 
 // 19e question
@@ -364,7 +364,7 @@ answer18.classList.toggle("ans3");
 const answer19=document.querySelector(".answ19");
 const button73=document.querySelector("#btn73");
 button73.addEventListener("click", ()=>{
-answer19.classList.toggle("ans3");
+answer19.classList.toggle("ans4");
 });
 const button75=document.querySelector("#btn75");
 button75.addEventListener("click", ()=>{
@@ -372,11 +372,11 @@ button75.addEventListener("click", ()=>{
 });
 const button76=document.querySelector("#btn76");
 button76.addEventListener("click", ()=>{
-answer19.classList.toggle("ans3");
+answer19.classList.toggle("ans4");
 });
 const button74=document.querySelector("#btn74");
 button74.addEventListener("click", ()=>{
-answer19.classList.toggle("ans3");
+answer19.classList.toggle("ans4");
 });
 
 
@@ -385,7 +385,7 @@ answer19.classList.toggle("ans3");
 const answer20=document.querySelector(".answ20");
 const button77=document.querySelector("#btn77");
 button77.addEventListener("click", ()=>{
-answer20.classList.toggle("ans3");
+answer20.classList.toggle("ans4");
 });
 const button78=document.querySelector("#btn78");
 button78.addEventListener("click", ()=>{
@@ -393,11 +393,11 @@ button78.addEventListener("click", ()=>{
 });
 const button79=document.querySelector("#btn79");
 button79.addEventListener("click", ()=>{
-answer20.classList.toggle("ans3");
+answer20.classList.toggle("ans4");
 });
 const button80=document.querySelector("#btn80");
 button80.addEventListener("click", ()=>{
-answer20.classList.toggle("ans3");
+answer20.classList.toggle("ans4");
 });
 
 // 21e question
@@ -405,7 +405,7 @@ answer20.classList.toggle("ans3");
 const answer21=document.querySelector(".answ21");
 const button81=document.querySelector("#btn81");
 button81.addEventListener("click", ()=>{
-answer21.classList.toggle("ans3");
+answer21.classList.toggle("ans4");
 });
 const button84=document.querySelector("#btn84");
 button84.addEventListener("click", ()=>{
@@ -413,11 +413,11 @@ button84.addEventListener("click", ()=>{
 });
 const button82=document.querySelector("#btn82");
 button82.addEventListener("click", ()=>{
-answer21.classList.toggle("ans3");
+answer21.classList.toggle("ans4");
 });
 const button83=document.querySelector("#btn83");
 button83.addEventListener("click", ()=>{
-answer21.classList.toggle("ans3");
+answer21.classList.toggle("ans4");
 });
 
 // 22e question
@@ -425,7 +425,7 @@ answer21.classList.toggle("ans3");
 const answer22=document.querySelector(".answ22");
 const button85=document.querySelector("#btn85");
 button85.addEventListener("click", ()=>{
-answer22.classList.toggle("ans3");
+answer22.classList.toggle("ans4");
 });
 const button88=document.querySelector("#btn88");
 button88.addEventListener("click", ()=>{
@@ -433,11 +433,11 @@ button88.addEventListener("click", ()=>{
 });
 const button87=document.querySelector("#btn87");
 button87.addEventListener("click", ()=>{
-answer22.classList.toggle("ans3");
+answer22.classList.toggle("ans4");
 });
 const button86=document.querySelector("#btn86");
 button86.addEventListener("click", ()=>{
-answer22.classList.toggle("ans3");
+answer22.classList.toggle("ans4");
 });
 
 // 23e question
@@ -445,7 +445,7 @@ answer22.classList.toggle("ans3");
 const answer23=document.querySelector(".answ23");
 const button89=document.querySelector("#btn89");
 button89.addEventListener("click", ()=>{
-answer23.classList.toggle("ans3");
+answer23.classList.toggle("ans4");
 });
 const button90=document.querySelector("#btn90");
 button90.addEventListener("click", ()=>{
@@ -453,11 +453,11 @@ button90.addEventListener("click", ()=>{
 });
 const button91=document.querySelector("#btn91");
 button91.addEventListener("click", ()=>{
-answer23.classList.toggle("ans3");
+answer23.classList.toggle("ans4");
 });
 const button92=document.querySelector("#btn92");
 button92.addEventListener("click", ()=>{
-answer23.classList.toggle("ans3");
+answer23.classList.toggle("ans4");
 });
 
 // 24e question
@@ -465,7 +465,7 @@ answer23.classList.toggle("ans3");
 const answer24=document.querySelector(".answ24");
 const button94=document.querySelector("#btn94");
 button94.addEventListener("click", ()=>{
-answer24.classList.toggle("ans3");
+answer24.classList.toggle("ans4");
 });
 const button93=document.querySelector("#btn93");
 button93.addEventListener("click", ()=>{
@@ -473,11 +473,11 @@ button93.addEventListener("click", ()=>{
 });
 const button95=document.querySelector("#btn95");
 button95.addEventListener("click", ()=>{
-answer24.classList.toggle("ans3");
+answer24.classList.toggle("ans4");
 });
 const button96=document.querySelector("#btn96");
 button96.addEventListener("click", ()=>{
-answer24.classList.toggle("ans3");
+answer24.classList.toggle("ans4");
 });
 
 // 25e question
@@ -485,7 +485,7 @@ answer24.classList.toggle("ans3");
 const answer25=document.querySelector(".answ25");
 const button98=document.querySelector("#btn98");
 button98.addEventListener("click", ()=>{
-answer25.classList.toggle("ans3");
+answer25.classList.toggle("ans4");
 });
 const button97=document.querySelector("#btn97");
 button97.addEventListener("click", ()=>{
@@ -493,9 +493,9 @@ button97.addEventListener("click", ()=>{
 });
 const button99=document.querySelector("#btn99");
 button99.addEventListener("click", ()=>{
-answer25.classList.toggle("ans3");
+answer25.classList.toggle("ans4");
 });
 const button100=document.querySelector("#btn100");
 button100.addEventListener("click", ()=>{
-answer25.classList.toggle("ans3");
+answer25.classList.toggle("ans4");
 });
