ff9e3ac5bf6ea02fee98f439076ef649c4d92264
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-07 13:11:25 +0100

    text

---
+++ javascript/script3.js
@@ -0,0 +1,7 @@
+const sr=ScrollReveal({
+    origin:"top",
+    duration:1500,
+    reset:true,
+    distance:"30px"
+})
+sr.reveal(".footer-text, .contact, .bd, .quizprog, .quiz, .about, .accueil, .reseaux", {interval:200})
