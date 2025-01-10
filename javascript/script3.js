17503f2834b42dc7fb2bfb6940ffd0e7bc751683
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-10 17:17:45 +0100

    javascript codage chargement de la page

--- javascript/script3.js
+++ javascript/script3.js
@@ -4,4 +4,13 @@ const sr=ScrollReveal({
     reset:true,
     distance:"30px"
 })
-sr.reveal(".footer-text, .contact, .bd, .quizprog, .quiz, .about, .accueil, .reseaux", {interval:200})
+sr.reveal(".footer-text, .contact, .bd, .quizprog, .quiz, .about, .accueil, .reseaux", {interval:200});
+
+// chargement de la page
+
+window.onload = function() {
+    document.querySelector('.loader').style.display = 'none';
+    document.querySelector('.loader').style.transition='5s ease-out'
+  };
+  
+  
